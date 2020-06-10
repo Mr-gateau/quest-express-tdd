7594bf3b27b6d84f62c8054ffc9437fcf34e0ea2
mr-gateau <tagot.nicolas@gmail.com>
2020-06-10 10:59:08 +0200

    mise en place du dotenv

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+const connection = require('./connection');
 
 const app = express();
 app.use(bodyParser.json());
